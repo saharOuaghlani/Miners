5c4d953b9b7f6d4b9996cd871ce10caeef11e421
ChahedOns <chahedons1@gmail.com>
2023-12-07 17:13:55 +0100

    ClassCordinator

--- src/main/Coordinator.java
+++ src/main/Coordinator.java
@@ -1,9 +1,49 @@
 package main;
 
 import jade.core.Agent;
+import java.util.List;
+
 
 public class Coordinator extends Agent{
 
-	public Coordinator(){super();};	
-	protected void setup(){System.out.println("Helloooooooo");}
+	
+	private int totalResources;
+	private int[][] resourceMatrix;
+	private int[][] discoveryMatrix;
+	private List<Explorer> robotExplorers;
+
+
+	public Coordinator CoordinatorAgent(int totalResources, int[][] resourceMatrix, int[][] discoveryMatrix, List<Explorer> robotExplorers) {
+        this.totalResources = totalResources;
+        this.resourceMatrix = resourceMatrix;
+        this.discoveryMatrix = discoveryMatrix;
+        this.robotExplorers = robotExplorers;
+    }
+
+    public void checkResources() {
+        if (areAllResourcesConsumed()) {
+            notifyEmptyResources();
+        }
+    }
+
+    private boolean areAllResourcesConsumed() {
+        for (int[] row : resourceMatrix) {
+            for (int value : row) {
+                if (value != 0) {
+                    return false;  // There are still resources
+                }
+            }
+        }
+        return true;  // All resources have been consumed
+    }
+
+    private void notifyEmptyResources() {
+        System.out.println("Rupture de ressources. Notifying robots...");
+        for (Explorer robot : robotExplorers) {
+            robot.handleEmptyResources();
+        }
+    }
 }
+
+
+
